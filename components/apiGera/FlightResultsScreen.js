c193d22886d946c23e1f7e98dc56f5586b211995
Gabriel Garcia <l22211932@tectijuana.edu.mx>
2024-11-08 15:29:48 -0800

    Update FlightResultsScreen.js
    Commit - Api Amadeus Credenciales

--- components/apiGera/FlightResultsScreen.js
+++ components/apiGera/FlightResultsScreen.js
@@ -26,16 +26,12 @@ const FlightResultsScreen = ({ route, navigation }) => {
     try {
       const response = await axios.post(
         "https://test.api.amadeus.com/v1/security/oauth2/token",
-        {
+        new URLSearchParams({
           grant_type: "client_credentials",
-          client_id: "YOUR_API_KEY",
-          client_secret: "YOUR_API_SECRET",
-        },
-        {
-          headers: {
-            "Content-Type": "application/x-www-form-urlencoded",
-          },
-        }
+          client_id: "FGDVkGl25Y5YCPjlGWxG5JCfEEVWfXqN",
+          client_secret: "rVxGDxaKm7BhAANl",
+        }),
+        { headers: { "Content-Type": "application/x-www-form-urlencoded" } }
       );
       return response.data.access_token;
     } catch (error) {
